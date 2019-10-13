c1f64007aeeb92f67a9468a148045a0edf95e894
Tatsuyasan <vincent.vermeersch2@gmail.com>
2019-10-13 22:19:56 +0200

    refactor, and property routes

--- src/routes/property.php
+++ src/routes/property.php
@@ -6,12 +6,21 @@
  * Notes:
  * Output-Formats: [application/json]
  */
-$app->POST('/v2/property', function ($request, $response, $args) {
 
+use Slim\Http\Request;
+use Slim\Http\Response;
 
-    $body = $request->getParsedBody();
-    $response->write('How about implementing createProperty as a POST method ?');
-    return $response;
+$app->POST('/v2/property', function (Request $request, Response $response, $args) {
+    try {
+        $dataRequest = $request->getParsedBody();
+        $insertStatement = $this->db->insert(array_keys($dataRequest))
+            ->into('property')
+            ->values(array_values($dataRequest));
+        $insertId = $insertStatement->execute(true);
+        return $response->withJson(['message' => 'Property number ' . $insertId . ' is create'], 201);
+    } catch (Exception $e) {
+        return $response->withJson(['message' => 'An error is occured'], 404);
+    }
 });
 
 
@@ -21,11 +30,27 @@ $app->POST('/v2/property', function ($request, $response, $args) {
  * Notes:
  * Output-Formats: [application/json]
  */
-$app->DELETE('/v2/property/{userId}', function ($request, $response, $args) {
+$app->DELETE('/v2/property/{idOwner}', function (Request $request, Response $response, $args) {
 
+    $sth = $this->db->prepare('
+DELETE 
+FROM property 
+JOIN `owner` ON property.id = `owner`.id_property
+WHERE `owner`.id = :idOwner;
+'
+    );
+    $sth->bindParam(':idOwner', $args['idOwner'], PDO::PARAM_INT);
+    $affectedRows = $sth->execute();
 
-    $response->write('How about implementing deleteProperty as a DELETE method ?');
-    return $response;
+    $sth = $this->db->prepare('
+UPDATE `owner` SET id_property = NULL
+ WHERE `owner`.id = :idOwner');
+    $sth->bindParam(':idOwner', $args['idOwner']);
+    $sth->execute();
+    if ($affectedRows)
+        return $response->withJson(['message' => 'All property of user is delete'], 200);
+
+    return $response->withJson(['message' => 'An error is occured'], 404);
 });
 
 
@@ -35,10 +60,19 @@ $app->DELETE('/v2/property/{userId}', function ($request, $response, $args) {
  * Notes:
  * Output-Formats: [application/json]
  */
-$app->GET('/v2/property/{userId}', function ($request, $response, $args) {
+$app->GET('/v2/property/{idOwner}', function ($request, $response, $args) {
+    $sth = $this->db->prepare('
+SELECT * FROM property
+JOIN `owner` ON property.id = `owner`.id_property
+WHERE `owner`.id = :idOwner');
+    $sth->bindParam(':idOwner', $args['idOwner']);
+    $sth->execute();
+    $data = $sth->fetchAll();
+    $response = $response->withJson($data);
 
+    if (empty($data))
+        return $response->withJson(['message' => 'Owner not found'], 404);
 
-    $response->write('How about implementing getAllPropertyByUserId as a GET method ?');
     return $response;
 });
 
@@ -49,11 +83,19 @@ $app->GET('/v2/property/{userId}', function ($request, $response, $args) {
  * Notes:
  * Output-Formats: [application/json]
  */
-$app->PUT('/v2/property', function ($request, $response, $args) {
+$app->PUT('/v2/property', function (Request $request, Response $response, $args) {
+    $dataRequest = $request->getParsedBody();
 
+    if (!isset($dataRequest['id']))
+        return $response->withJson(['message' => 'Property\'s ID is required'], 404);
 
-    $body = $request->getParsedBody();
-    $response->write('How about implementing updatePropertyById as a PUT method ?');
-    return $response;
+    $updateStatement = $this->db->update($dataRequest)
+        ->table('property')
+        ->where('id', '=', $dataRequest['id']);
+    $affectedRows = $updateStatement->execute();
+    if ($affectedRows)
+        return $response->withJson(['message' => 'Property is update'], 200);
+
+    return $response->withJson(['message' => 'Property not found or no update data'], 404);
 });
 
--- src/routes/user.php
+++ src/routes/user.php
@@ -9,10 +9,20 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-$app->GET('/v2/getRentersByOwnerId/{id}', function ($request, $response, $args) {
+$app->GET('/v2/getRentersByOwnerId/{idOwner}', function ($request, $response, $args) {
+    $sth = $this->db->prepare('
+SELECT renter.id, renter.firstname, renter.lastname, renter.mail, renter.password, renter.date_creation, renter.date_modification, renter.phone, renter.adress, renter.additional_adress
+FROM renter 
+JOIN property ON renter.id = property.id_renter
+JOIN `owner`  ON property.id = `owner`.id_property
+WHERE `owner`.id = :idOwner');
+    $sth->bindParam(':idOwner', $args['idOwner'], PDO::PARAM_INT);
+    $sth->execute();
+    $data = $sth->fetchAll();
+    $response = $response->withJson($data);
+    if (empty($data))
+        return $response->withJson(['message' => 'User not found'], 404);
 
-
-    $response->write('How about implementing getAllRentersOfOwner as a GET method ?');
     return $response;
 });
 
@@ -23,20 +33,33 @@ $app->GET('/v2/getRentersByOwnerId/{id}', function ($request, $response, $args) 
  * Notes: This can only be done by the logged in user.
  * Output-Formats: [application/json]
  */
-$app->POST('/v2/user', function (Request $request, Response $response) {
+$app->POST('/v2/owner', function (Request $request, Response $response) {
     try {
         $dataRequest = $request->getParsedBody();
         $insertStatement = $this->db->insert(array_keys($dataRequest))
-            ->into('users')
+            ->into('owner')
             ->values(array_values($dataRequest));
         $insertId = $insertStatement->execute(true);
-        return $response->withJson(['message' => 'User number ' . $insertId . ' is create'], 201);
+        return $response->withJson(['message' => 'Owner number ' . $insertId . ' is create'], 201);
     } catch (Exception $e) {
         return $response->withJson(['message' => 'An error is occured'], 404);
     }
 
 });
 
+$app->POST('/v2/renter', function (Request $request, Response $response) {
+    try {
+        $dataRequest = $request->getParsedBody();
+        $insertStatement = $this->db->insert(array_keys($dataRequest))
+            ->into('renter')
+            ->values(array_values($dataRequest));
+        $insertId = $insertStatement->execute(true);
+        return $response->withJson(['message' => 'Renter number ' . $insertId . ' is create'], 201);
+    } catch (Exception $e) {
+        return $response->withJson(['message' => 'An error is occured'], 404);
+    }
+
+});
 
 /**
  * DELETE deleteUser
@@ -44,17 +67,33 @@ $app->POST('/v2/user', function (Request $request, Response $response) {
  * Notes: This can only be done by the logged in user.
  * Output-Formats: [application/json]
  */
-$app->DELETE('/v2/user/{id}', function (Response $response, $args) {
+$app->DELETE('/v2/owner/{id}', function (Request $request, Response $response, $args) {
     $deleteStatement = $this->db->delete()
-        ->from('users')
+        ->from('owner')
         ->where('id', '=', $args['id']);
     $affectedRows = $deleteStatement->execute();
     if ($affectedRows)
-        return $response->withJson(['message' => 'User is delete'], 200);
+        return $response->withJson(['message' => 'Owner is delete'], 200);
 
     return $response->withJson(['message' => 'An error is occured'], 404);
 });
 
+/**
+ * DELETE deleteUser
+ * Summary: Delete user
+ * Notes: This can only be done by the logged in user.
+ * Output-Formats: [application/json]
+ */
+$app->DELETE('/v2/renter/{id}', function (Request $request, Response $response, $args) {
+    $deleteStatement = $this->db->delete()
+        ->from('renter')
+        ->where('id', '=', $args['id']);
+    $affectedRows = $deleteStatement->execute();
+    if ($affectedRows)
+        return $response->withJson(['message' => 'Renter is delete'], 200);
+
+    return $response->withJson(['message' => 'An error is occured'], 404);
+});
 
 /**
  * GET getDashboardInfoById
@@ -62,10 +101,23 @@ $app->DELETE('/v2/user/{id}', function (Response $response, $args) {
  * Notes:
  * Output-Formats: [application/json]
  */
-$app->GET('/v2/user/getDashboardInfoById/{id}', function ($request, $response, $args) {
+$app->GET('/v2/user/getDashboardInfoOwnerById/{id}', function (Request $request, Response $response, $args) {
 
 
-    $response->write('How about implementing getDashboardInfoById as a GET method ?');
+    $response->write('How about implementing getDashboardInfoOwnerById as a GET method ?');
+    return $response;
+});
+
+/**
+ * GET getDashboardInfoById
+ * Summary: Get info dashboard by user id
+ * Notes:
+ * Output-Formats: [application/json]
+ */
+$app->GET('/v2/user/getDashboardInfoByRenterId/{id}', function (Request $request, Response $response, $args) {
+
+
+    $response->write('How about implementing getDashboardInfoByRenterId as a GET method ?');
     return $response;
 });
 
@@ -76,16 +128,36 @@ $app->GET('/v2/user/getDashboardInfoById/{id}', function ($request, $response, $
  * Notes:
  * Output-Formats: [application/json]
  */
-$app->GET('/v2/user/{id}', function (Response $response, $args) {
+$app->GET('/v2/owner/{idOwner}', function (Request $request, Response $response, $args) {
     $selectStatement = $this->db->select()
-        ->from('users')
+        ->from('owner')
+        ->where('id', '=', $args['idOwner']);
+    $stmt = $selectStatement->execute();
+    $data = $stmt->fetch();
+    $response = $response->withJson($data);
+
+    if (empty($data))
+        return $response->withJson(['message' => 'Owner not found'], 404);
+
+    return $response;
+});
+
+/**
+ * GET getUserById
+ * Summary: Get user by user id
+ * Notes:
+ * Output-Formats: [application/json]
+ */
+$app->GET('/v2/renter/{id}', function (Request $request, Response $response, $args) {
+    $selectStatement = $this->db->select()
+        ->from('renter')
         ->where('id', '=', $args['id']);
     $stmt = $selectStatement->execute();
     $data = $stmt->fetch();
     $response = $response->withJson($data);
 
     if (empty($data))
-        return $response->withJson(['message' => 'User not found'], 404);
+        return $response->withJson(['message' => 'Renter not found'], 404);
 
     return $response;
 });
@@ -97,20 +169,42 @@ $app->GET('/v2/user/{id}', function (Response $response, $args) {
  * Notes: This can only be done by the logged in user.
  * Output-Formats: [application/json]
  */
-$app->PUT('/v2/user', function (Request $request, Response $response) {
+$app->PUT('/v2/owner', function (Request $request, Response $response, $args) {
+    var_dump('toto');exit;
     $dataRequest = $request->getParsedBody();
 
     if (!isset($dataRequest['id']))
-        return $response->withJson(['message' => 'User\'s ID is required'], 404);
+        return $response->withJson(['message' => 'Owner\'s ID is required'], 404);
 
     $updateStatement = $this->db->update($dataRequest)
-        ->table('users')
+        ->table('owner')
         ->where('id', '=', $dataRequest['id']);
     $affectedRows = $updateStatement->execute();
     if ($affectedRows)
-        return $response->withJson(['message' => 'User is update'], 200);
+        return $response->withJson(['message' => 'Owner is update'], 200);
 
-    return $response->withJson(['message' => 'User not found or no update data'], 404);
+    return $response->withJson(['message' => 'Owner not found or no update data'], 404);
 });
 
+/**
+ * PUT updateUser
+ * Summary: Updated user
+ * Notes: This can only be done by the logged in user.
+ * Output-Formats: [application/json]
+ */
+$app->PUT('/v2/renter', function (Request $request, Response $response, $args) {
+    $dataRequest = $request->getParsedBody();
+
+    if (!isset($dataRequest['id']))
+        return $response->withJson(['message' => 'Renter\'s ID is required'], 404);
+
+    $updateStatement = $this->db->update($dataRequest)
+        ->table('renter')
+        ->where('id', '=', $dataRequest['id']);
+    $affectedRows = $updateStatement->execute();
+    if ($affectedRows)
+        return $response->withJson(['message' => 'Renter is update'], 200);
+
+    return $response->withJson(['message' => 'Renter not found or no update data'], 404);
+});
 
